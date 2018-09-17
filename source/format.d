6f7d2b6a2bdb21a467990ee46d2e73fc60450a38
pphop <1um6okote@gmail.com>
2018-09-18 00:56:04 +0500

    added a simple custom formatter

---
+++ source/format.d
@@ -0,0 +1,74 @@
+module pepel.format;
+
+struct Formatter(T, string[] fmtmappings)
+        if (mappingIsValid!(T, fmtmappings)) {
+    import std.algorithm : map;
+    import std.array : split;
+    import std.string : strip;
+    import std.regex : regex;
+
+    // doesnt work for some reason, seems to be a problem with a compiler
+    // private alias parsePairs = map!(a => a.split(">").map!strip);
+    // static foreach (mapping; parsePairs(fmtmappings)) {
+    // mixin(`private auto _regex` ~ mapping[1] ~ ` = ctRegex!("{` ~ mapping[0] ~ `}");`);
+    // }
+    mixin(generateRegexFields!fmtmappings);
+
+    string format(string fmt, T value) {
+        import std.regex : replaceAll;
+
+        static foreach (mapping; fmtmappings.map!(a => a.split(">").map!strip)) {
+            fmt = fmt.replaceAll!(m => mixin("value." ~ mapping[1]))(mixin("_regex" ~ mapping[1]));
+        }
+        return fmt;
+    }
+}
+
+private template generateRegexFields(string[] mappings) {
+    enum generateRegexFields = gen();
+
+    private string gen() {
+        import std.algorithm : joiner, map;
+        import std.array : split;
+        import std.conv : to;
+        import std.string : format, strip;
+
+        string res = mappings.map!(a => a.split(">").map!strip)
+            .map!(a => format!`private static _regex%s = regex("\\{%s\\}");`(a[1], a[0]))
+            .joiner("\n").to!string;
+        return res;
+    }
+}
+
+private bool mappingIsValid(T, string[] mappings)() {
+    import std.algorithm : map;
+    import std.array : array, split;
+    import std.string : format, strip;
+
+    static foreach (mapping; mappings) {
+        {
+            enum mSplit = mapping.split(">").map!strip.array;
+            static assert(mSplit.length == 2, "mapping syntax is invalid");
+            static assert(__traits(compiles, mixin("T." ~ mSplit[1])),
+                    format!"there is no '%s' field for type %s"(mSplit[1], T.stringof));
+        }
+    }
+    return true;
+}
+
+unittest {
+    struct S {
+        string a;
+        string b;
+    }
+
+    assert(!__traits(compiles, Formatter!(S, ["qwe > fieldThatDoesntExist"])()));
+
+    auto f = Formatter!(S, ["fielda > a", "s.fieldb > b"])();
+
+    auto s1 = S("qwe", "rty");
+    auto fmtstr = "{fielda} {s.fieldb}";
+    auto expected = "qwe rty";
+    auto got = f.format(fmtstr, s1);
+    assert(got == expected);
+}

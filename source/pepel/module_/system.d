9acd3c185d2c7e268e2e797324bfe1f510a29bd9
pphop <1um6okote@gmail.com>
2019-05-12 03:10:36 +0500

    added a few system module commands

--- source/pepel/module_/system.d
+++ source/pepel/module_/system.d
@@ -1,19 +1,37 @@
 module pepel.module_.system;
 
+import std.datetime.systime;
+
 import pepel.common;
 import pepel.module_.module_;
 
-class SystemModule : Module {
+final class SystemModule : Module {
+
+    SysTime startTime;
 
     this() {
         mixin(generateCommands!SystemModule);
+        startTime = Clock.currTime;
     }
 
-    @command("ping") NR ping(ref Message) {
+    @command("ping") NR ping(ref Message msg) {
+        msg.handled = true;
         return NR(Response("pong"));
     }
 
-    @command("test", User.Role.moderator) NR test(ref Message) {
-        return NR(Response("you are a mod pog"));
+    @command("uptime") NR uptime(ref Message msg) {
+        import std.format : format;
+
+        msg.handled = true;
+        return NR(Response("running for %s".format(Clock.currTime - startTime)));
+    }
+
+    @command("shutdown", User.Role.botowner) NR shutdown(ref Message msg) {
+        import std.format : format;
+        import vibe.core.core : exitEventLoop;
+
+        msg.handled = true;
+        exitEventLoop();
+        return NR(Response("shutting down after running for %s".format(Clock.currTime - startTime)));
     }
 }
